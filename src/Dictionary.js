cd6e876758db9a07e6aeda7ed89925142bac9bd2
radevden <radevden@gmail.com>
2024-12-04 16:57:06 +0100

    added axios + api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import './App.css';
+import axios from "axios"
 
 export default function Dictionary (){
     let [word, searchWord] = useState(null);
+    
+    function handleResponse(response){
+            console.log(response);
+    }
     
     function handleSubmit (event){
         event.preventDefault();
         console.log();
         alert(`Searching for ${word}`);
+
+        let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4"
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function displaySearch (event){
