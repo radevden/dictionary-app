b4e61de79cc4742b7ab312cfdc7aedbf7dc2247a
radevden <radevden@gmail.com>
2024-12-28 23:17:18 +0100

    pexels API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,12 +11,21 @@ export default function Dictionary (){
             setResults(response.data);
     }
     
+    function handlePexelsResponse(response){
+        console.log(response.data);
+    }
+
     function handleSubmit (event){
         event.preventDefault();
 
         let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4"
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApi = "VZy5kWaJYQWL61SU0tnv1gKLYmhl4dSkPxeKqrbGAXmGUZq1xjjOfBv4";
+        let pexelsUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=1`;
+        let headers = { Authorization: `Bearer ${pexelsApi}` };
+        axios.get(pexelsUrl, { headers: headers }).then(handlePexelsResponse).catch((error) => console.error("Pexels API Error:", error));
     }
 
     function displaySearch (event){
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Synonyms(props){
    return (<div>
-    {props.synonyms === null ? null : (<ul className="synonyms">Synonyms:{props.synonyms.map(function(synonym, index){
+    {props.synonyms === null ? null : (<ul className="synonyms">Similar:{props.synonyms.map(function(synonym, index){
         return (<li key={index} className="synonymList">{synonym}</li>)
     })}</ul>)}
    </div>)
